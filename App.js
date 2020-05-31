464244884b38b357fa5de5ae84aa3fcfbf434e34
MaxTD <32356302+MaxTD@users.noreply.github.com>
2020-05-31 21:17:56 +1000

    Fixed Button "Now locates where you're

--- App.js
+++ App.js
@@ -1,69 +1,228 @@
-// IOS v1.3
+// IOS v1.7
 
 import React, { useState, useEffect, Component } from 'react';
 import {
+  Button,
+  Text,
+  Animated,
+  TouchableWithoutFeedback,
   ActivityIndicator,
   AppRegistry,
   StyleSheet,
   View,
+  Dimensions,
 } from 'react-native';
 import MapView, { PROVIDER_DEFAULT } from 'react-native-maps';
-import FloatingButton from "./Test/FloatingButton";
+import { AntDesign, Entypo } from "@expo/vector-icons";
 
-// export { AppContainer, TabNavigator }
+class FloatingButton extends React.Component {
 
-const initialState = {
-  latitude: null,
-  longitude: null,
-  latitudeDelta: 0.0922,
-  longitudeDelta: 0.0421,
-}
-const App = () => {
-  const [curentPosition, setCurentPosition] = useState(initialState);
+  animation = new Animated.Value(0)
 
-  useEffect(() => {
-    navigator.geolocation.getCurrentPosition(position => {
-      // alert(JSON.stringify(position))
-      const { longitude, latitude } = position.coords;
-    setCurentPosition({
-      ...curentPosition,
-      latitude,
-      longitude,
-    })
-  },
-      error => alert(error.message),
-      { timeout: 20000, maximumAge: 1000}
-    )
-  }, [])
-  
-  return curentPosition.latitude ? (
-    <View style={styles.containerTop}>
-      <View style={styles.container}>
-        <MapView
-          provider={PROVIDER_DEFAULT}
-          style={styles.map}
-          showsUserLocation={true}
-          initialRegion={curentPosition}>
-        </MapView>
+  constructor(props) {
+    super(props);
+  }
 
+  _onPress() {
+    console.log("Pinned")
+  }
 
-        <View style={{
-          backgroundColor: "black", height: "10%", width: "100%",
-          alignSelf: "flex-end", borderTopLeftRadius: "25", borderTopRightRadius: "25",
-        }}>
-        </View>
-        <FloatingButton style={{ bottom: 100 }} />
+  toggleMenu = () => {
+    const toValue = this.open ? 0 : 1;
+
+    Animated.spring(this.animation, {
+      toValue,
+      friction: 5
+    }).start();
+
+    this.open = !this.open;
+  }
+
+  render() {
+    const pinStyle = {
+      transform: [
+        { scale: this.animation },
+        {
+          translateY: this.animation.interpolate({
+            inputRange: [0, 1],
+            outputRange: [0, -80]
+          })
+        }
+      ]
+    };
+
+    const thumbStyle = {
+      transform: [
+        { scale: this.animation },
+        {
+          translateY: this.animation.interpolate({
+            inputRange: [0, 1],
+            outputRange: [0, -140]
+          })
+        }
+      ]
+    };
+
+    const rotation = {
+      transform: [
+        {
+          rotate: this.animation.interpolate({
+            inputRange: [0, 1],
+            outputRange: ["0deg", "45deg"]
+          })
+        }
+      ]
+    };
+
+    return (
+      <View style={[styles.container, this.props.style]}>
+        <TouchableWithoutFeedback>
+          <Animated.View style={[styles.button, styles.secondary, thumbStyle]}>
+            <Entypo onPress={() => this.props.onLocate()} name="hair-cross" size={20} color="black" />
+          </Animated.View>
+        </TouchableWithoutFeedback>
+
+        <TouchableWithoutFeedback>
+          <Animated.View style={[styles.button, styles.secondary, pinStyle]}>
+            <Entypo onPress={() => this._onPress()} name="location-pin" size={20} color="black" />
+          </Animated.View>
+        </TouchableWithoutFeedback>
+
+        <TouchableWithoutFeedback onPress={this.toggleMenu}>
+          <Animated.View style={[styles.button, styles.menu, rotation]}>
+            <AntDesign name="plus" size={24} color="black" />
+          </Animated.View>
+        </TouchableWithoutFeedback>
       </View>
-    </View>
-  ) : <ActivityIndicator style={{ flex: 1 }} animating size="large"/>
-};
+    );
+  }
+}
+
+
+
+export default class App extends React.Component {
+
+  // export default class App extends Component {
+
+
+  state = {
+
+    focusedLocation: {
+      latitude: 37.7900352,
+      longitude: -122.4013726,
+      latitudeDelta: 0.0122,
+      longitudeDelta:
+        Dimensions.get("window").width /
+        Dimensions.get("window").height *
+        0.0122
+    },
+    locationChosen: false
+  }
+
+  pickLocationHandler = event => {
+    const coords = event.nativeEvent.coordinate;
+    this.map.animateToRegion({
+      ...this.state.focusedLocation,
+      latitude: coords.latitude,
+      longitude: coords.longitude
+    });
+    this.setState(prevState => {
+      return {
+        focusedLocation: {
+          ...prevState.focusedLocation,
+          latitude: coords.latitude,
+          longitude: coords.longitude
+        },
+        locationChosen: true
+      };
+    });
+
+  };
+
+  getLocationHandler = () => {
+    navigator.geolocation.getCurrentPosition(pos => {
+      const coordsEvent = {
+        nativeEvent: {
+          coordinate: {
+            latitude: pos.coords.latitude,
+            longitude: pos.coords.longitude
+          }
+        }
+      };
+      this.pickLocationHandler(coordsEvent);
+    },
+      err => {
+        console.log(err);
+        alert("Fetching the Position failed, please pick one manually!");
+      })
+  }
+
+
+  render() {
+    // let marker = null;
+
+    // if (this.state.locationChosen) {
+    //   marker = <MapView.Marker coordinate={this.state.focusedLocation} />;
+    // }
+
+    return (
+      <View style={styles.containerTop}>
+        <View style={styles.container1}>
+          <MapView
+            initialRegion={this.state.focusedLocation}
+            region={!this.state.locationChosen ? this.state.focusedLocation : null}
+            style={styles.map}
+            onPress={this.pickLocationHandler}
+            ref={ref => this.map = ref}
+          showsUserLocation={true}
+          >
+            {/* {marker} */}
+          </MapView>
+
+          <View style={{
+            backgroundColor: "black", height: "10%", width: "100%",
+            alignSelf: "flex-end", borderTopLeftRadius: "25", borderTopRightRadius: "25",
+          }}>
+          </View>
+          <FloatingButton onLocate={this.getLocationHandler} style={{ bottom: 100 }} />
+        </View>
+      </View>
+    );
+  }
+}
+
 
 const styles = StyleSheet.create({
+  container: {
+    alignItems: "center",
+    position: "absolute"
+  },
+  button: {
+    position: "absolute",
+    width: 60,
+    height: 60,
+    borderRadius: 60 / 2,
+    alignItems: "center",
+    justifyContent: "center",
+    shadowRadius: 10,
+    shadowColor: "#FFF",
+    shadowOpacity: 0.3,
+    shadowOffset: { height: 10 }
+  },
+  menu: {
+    backgroundColor: "#FFF"
+  },
+  secondary: {
+    width: 48,
+    height: 48,
+    borderRadius: 48 / 2,
+    backgroundColor: "#FFF"
+  },
   containerTop: {
     borderRadius: 6,
     flex: 1,
   },
-  container: {
+  container1: {
     display: "flex",
     height: "100%",
     flex: 1,
@@ -89,7 +248,3 @@ const styles = StyleSheet.create({
     marginBottom: 5,
   },
 });
-
-AppRegistry.registerComponent('FindMyParkedCarMaps', () => FindMyParkedCarMaps);
-
-export default App;
